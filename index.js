4562ea1a41d8fbb5190c987899e67b8152497639
Maicol Duque <maicol.losdelsur@hotmail.com>
2019-01-30 12:02:13 -0500

    Se agrego un modelo y se craron los servicios principales para el CRUD de productos

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express") //Es un framework para node js
 const bodyParser = require("body-parser") //Body parser es un middleware
 const mongoose = require("mongoose")
 
+const Product = require("./models/product")
+
 const app = express();
 const port = process.env.PORT || 3000
 
@@ -12,23 +14,68 @@ app.use(bodyParser.json())
 
 
 app.get('/api/product', (req, res) => {
-    res.send(200, { products: [] })
+    Product.find({}, (err, products) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la petición ${err}` })
+        if (!products) return res.status(404).send({ message: "No existen prodcutos!" })
+        
+        res.send(200, { products })
+    })
 })
 
 app.get('/api/product/:id', (req, res) => {
+    let productID = req.params.id 
 
+    Product.findById(productID, (err, product) => {
+        if(err) return res.status(500).send({message: `Error al realizar la petición ${err}`})
+        if(!product) return res.status(404).send({message: "El producto no existe.."})
+
+        res.status(200).send({product})
+    })
 })
 
 app.post("/api/product", (req, res) => {
+    console.log('POST  /api/product')
     console.log(req.body)
-    res.status(200).send({ mensaje: "el productos se ha recibido.." })
+
+    let product = new Product()
+    product.name    = req.body.name
+    product.picture = req.body.picture
+    product.price   = req.body.price
+    product.category = req.body.category
+    product.description = req.body.description
+
+    product.save((err, productStore) => {
+        if(err) res.status(500).send({mensaje: 'Error al guardar'})
+        res.status(200).send({ product: productStore })
+    })
+
 })
 
+
+//Actualizar
 app.put("/api/product/:id", (req, res) => {
-
+    let productID = req.params.id 
+    let body = req.body 
+    Product.findByIdAndUpdate(productID, body, (err, productUpdated) => {
+        if (err) res.status(500).send({ mensaje: 'Error al actualizar el producto' })
+        
+        res.status(200).send({ product: productUpdated })        
+    })
 })
 
+
+
+//Eliminar
 app.delete("/api/product/:id", (req, res) => {
+    let productID = req.params.id 
+    Product.findById(productID, (err, product) => {
+        if (err) return res.status(500).send({ message: `Error al borrar el producto: ${err}` })
+        product.remove(err => {
+            if (err) return res.status(500).send({ message: `Error al borrar el producto: ${err}` })            
+            res.status(200).send({ message: 'Prodcuto borrado exitosamente!' })
+        })
+
+    })
 
 })
 
